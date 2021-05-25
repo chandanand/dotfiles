e19e63c89be024f4c14a3fd3e48ca89b852ad27e
Chand Anand <canand@uber.com>
2021-05-24 17:44:44 -0700

    feat(fzf): set defaults

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -3,3 +3,16 @@ fish_add_path $HOME/.bin
 # config paths
 set -gx BAT_CONFIG_PATH "~/.config/bat/config"
 set -gx RIPGREP_CONFIG_PATH "~/.rgrc"
+
+# defaults
+# based on https://github.com/junegunn/fzf#3-interactive-ripgrep-integration
+set -gx INITIAL_QUERY ""
+set -gx RG_PREFIX "rg --column --line-number --no-heading --color=always --smart-case "
+set -gx FZF_DEFAULT_COMMAND "$RG_PREFIX '$INITIAL_QUERY'" \
+  fzf --bind "change:reload:$RG_PREFIX {q} || true" \
+      --ansi --disabled --query "$INITIAL_QUERY" \
+      --height=50% --layout=reverse
+set -gx FZF_DEFAULT_OPTS '--color=dark' \
+'--color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe' \
+'--color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef' \
+'--cycle --reverse --border --height 50% --preview-window=wrap'
--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -24,18 +24,6 @@ if not set --query fzf_fish_custom_keybindings
     end
 end
 
-# If FZF_DEFAULT_OPTS is not set, then set some sane defaults. This also affects fzf outside of this plugin.
-# See https://github.com/junegunn/fzf#environment-variables
-if not set --query FZF_DEFAULT_OPTS
-    # cycle allows jumping between the first and last results, making scrolling faster
-    # layout=reverse lists results top to bottom, mimicking the familiar layouts of git log, history, and env
-    # border makes clear where the fzf window begins and ends
-    # height=90% leaves space to see the current command and some scrollback, maintaining context of work
-    # preview-window=wrap wraps long lines in the preview window, making reading easier
-    # marker=* makes the multi-select marker more distinguishable from the pointer (since both default to >)
-    set --global --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
-end
-
 function _fzf_uninstall --on-event fzf_uninstall
     # Not going to erase FZF_DEFAULT_OPTS because too hard to tell if it was set by the user or by this plugin
     if not set --query fzf_fish_custom_keybindings
