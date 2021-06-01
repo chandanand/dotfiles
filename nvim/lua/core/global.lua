e03e8d21eb108303c8082e980e8617733dd6f77f
Chand Anand <canand@uber.com>
2021-06-01 15:38:22 -0700

    feat(nvim): instantiate lua config

---
+++ nvim/lua/core/global.lua
@@ -0,0 +1,9 @@
+local global = {}
+local home = os.getenv("HOME")
+
+function global:load_variables()
+end
+
+global:load_variables()
+
+return global
---
+++ nvim/lua/core/init.lua
@@ -0,0 +1,30 @@
+local disable_distribution_plugins = function()
+  vim.g.loaded_gzip = 1
+  vim.g.loaded_tar = 1
+  vim.g.loaded_tarPlugin = 1
+  vim.g.loaded_zip = 1
+  vim.g.loaded_zipPlugin = 1
+  vim.g.loaded_getscript = 1
+  vim.g.loaded_getscriptPlugin = 1
+  vim.g.loaded_vimball = 1
+  vim.g.loaded_vimballPlugin = 1
+  vim.g.loaded_matchit = 1
+  vim.g.loaded_matchparen = 1
+  vim.g.loaded_2html_plugin = 1
+  vim.g.loaded_logiPat = 1
+  vim.g.loaded_rrhelper = 1
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+end
+
+local map_leader = function()
+  vim.api.nvim_set_keymap('n',' ','',{noremap = true})
+  vim.api.nvim_set_keymap('x',' ','',{noremap = true})
+  vim.g.mapleader = " "
+end
+
+local load_core = function()
+  map_leader()
+end
+
+load_core()
