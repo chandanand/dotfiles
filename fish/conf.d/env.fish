a41ecb0b2c06b6a605cbcc01b7b49e103b52336c
Chand Anand <canand@uber.com>
2021-07-22 14:44:17 -0700

    chore(fish): update fzf plugin

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -4,15 +4,32 @@ fish_add_path $HOME/.bin
 set -gx BAT_CONFIG_PATH "$HOME/.config/bat/config"
 set -gx RIPGREP_CONFIG_PATH "$HOME/.rgrc"
 
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+set -gx GOPATH $HOME/gocode
+
 # defaults
 # based on https://github.com/junegunn/fzf#3-interactive-ripgrep-integration
 set -gx INITIAL_QUERY ""
 set -gx RG_PREFIX "rg --column --line-number --no-heading --color=always --smart-case "
 set -gx FZF_DEFAULT_COMMAND "$RG_PREFIX '$INITIAL_QUERY'" \
   fzf --bind "change:reload:$RG_PREFIX {q} || true" \
-      --ansi --disabled --query "$INITIAL_QUERY" \
-      --height=50% --layout=reverse
-set -gx FZF_DEFAULT_OPTS '--color=dark' \
-'--color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe' \
-'--color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef' \
-'--cycle --reverse --border --height 50% --preview-window=wrap'
+      --ansi --disabled --query "$INITIAL_QUERY"
+set -gx FZF_DEFAULT_OPTS "
+--layout=reverse
+--info=inline
+--height=50%
+--border
+--multi
+--preview-window=:hidden
+--color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008'
+--prompt='∼ ' --pointer='▶' --marker='✓'
+--bind '?:toggle-preview'
+--bind 'ctrl-a:select-all'
+--bind 'ctrl-y:execute-silent(echo {+} | pbcopy)'
+--bind 'ctrl-e:execute(echo {+} | xargs -o nvim)'
+--bind 'ctrl-v:execute(code {+})'
+"
+
+set -g fzf_preview_dir_cmd "ls --tree -A -F"
